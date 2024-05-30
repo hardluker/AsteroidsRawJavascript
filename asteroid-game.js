03fba0f254573a7b5b9d6553515ef922ecf75917
hardluker <dba9514@gmail.com>
2024-05-30 12:39:32 -0400

    cleaned up the main game file for readability and maintainability.

--- asteroid-game.js
+++ asteroid-game.js
@@ -4,30 +4,43 @@ import AsteroidBelt from './asteroidBelt.js'; // Import the AsteroidBelt class
 // Game Settings
 const FPS = 30; // Frames per Second
 
+// Ship Related Settings
 const SHIP_SIZE = 30; // Ship height in pixels
 const SHIP_THRUST = 8; // Acceleration of ship in pixels per second every second
-const FRICTION = 0.7; // Friction coefficient of space (0 = no friction, 1 = ton of friction)
+const FRICTION = 0.4; // Friction coefficient of space (0 = no friction, 1 = ton of friction)
 const TURN_SPEED = 360; // Turn speed in degrees per second
 const SHIP_EXPLODE_DURATION = 0.3; // Duration of ship explosion in seconds
 
+// Development Tools
 const SHOW_CENTER_DOT = false; //Development tool for visualizing ship center and trajectory.
-const SHOW_BOUNDING = true; // Development tool to visualize collision bounding
+const SHOW_BOUNDING = false; // Development tool to visualize collision bounding
 
+// Asteroid Related Settings
 const ASTEROIDS_NUM = 3; //Number of asteroids at the starting level.
 const ASTEROIDS_SIZE = 100; // Starting size of asteroids in pixels
 const ASTEROIDS_SPEED = 50; // Max starting speed in pixels per second.
 const ASTEROIDS_VERT = 10; // Average number of vertices of the asteroids.
 const ASTEROIDS_JAGGEDNESS = 0.4; //Jaggeness of asteroids. ( 0 = none, 1 = ton of)
 
+//Defining the canvas and context for drawing the game.
 /** @type {HTMLCanvasElement} */
 let canv = document.getElementById('gameCanvas');
 let context = canv.getContext('2d');
 
-//Creating a ship
-let ship = new Ship(canv, context);
-
 // Set up the Game loop
-setInterval(update, 1000 / FPS);
+setInterval(gameLoop, 1000 / FPS);
+
+//Creating a ship
+let ship = new Ship(
+  canv,
+  context,
+  SHIP_SIZE,
+  SHIP_THRUST,
+  FRICTION,
+  TURN_SPEED,
+  FPS,
+  SHIP_EXPLODE_DURATION
+);
 
 // Create an instance of AsteroidBelt after the ship object is fully initialized
 let asteroidBelt = new AsteroidBelt(
@@ -39,28 +52,32 @@ let asteroidBelt = new AsteroidBelt(
   ASTEROIDS_SPEED,
   ASTEROIDS_VERT,
   ASTEROIDS_JAGGEDNESS,
-  ship // Pass ship object
+  ship
 );
 
-function explodeShip() {
-  ship.explodeTime = Math.ceil(SHIP_EXPLODE_DURATION * FPS);
-}
-
-export function distBetweenPoints(x1, y1, x2, y2) {
-  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-}
-
-function update() {
-  ship.exploding = ship.explodeTime > 0;
-
+function gameLoop() {
   // Draw Outer Space Background
   context.fillStyle = 'black';
   context.fillRect(0, 0, canv.width, canv.height);
 
   // Updating ship positional and logical data.
-  ship.update(FPS);
+  ship.update();
+
+  // Update Asteroid belt positional and logical data.
+  asteroidBelt.update();
 
   // Check for asteroid collisions
+  detectCollisions();
+
+  //Drawing the ship
+  ship.draw(SHOW_CENTER_DOT, SHOW_BOUNDING);
+
+  //Drawing the asteroid belt
+  asteroidBelt.draw(SHOW_BOUNDING);
+}
+
+// Function for detecting collisions with the ship and the asteroids
+function detectCollisions() {
   for (let i = 0; i < asteroidBelt.asteroids.length; i++) {
     if (
       distBetweenPoints(
@@ -71,14 +88,12 @@ function update() {
       ) <
       ship.r + asteroidBelt.asteroids[i].r
     ) {
-      explodeShip();
+      ship.explode();
     }
   }
+}
 
-  //Drawing the ship
-  ship.draw(SHOW_CENTER_DOT, SHOW_BOUNDING);
-
-  // Update and draw the asteroid belt
-  asteroidBelt.update();
-  asteroidBelt.draw(SHOW_BOUNDING);
+// Utility function for checking the distance between 2 objects
+export function distBetweenPoints(x1, y1, x2, y2) {
+  return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 }
--- ship.js
+++ ship.js
@@ -1,5 +1,14 @@
 class Ship {
-  constructor(canv, context) {
+  constructor(
+    canv,
+    context,
+    SHIP_SIZE,
+    SHIP_THRUST,
+    FRICTION,
+    TURN_SPEED,
+    FPS,
+    SHIP_EXPLODE_DURATION
+  ) {
     //Canvas / document related attributes
     this.canv = canv;
     this.context = context;
@@ -7,17 +16,19 @@ class Ship {
     //Positional Attributes
     this.x = canv.width / 2; // Ship always starts in the middle of the screen
     this.y = canv.height / 2;
-    this.size = 30; // Ship height in pixels
+    this.size = SHIP_SIZE; // Ship height in pixels
     this.r = this.size / 2;
     this.a = (90 / 180) * Math.PI; // Convert degree to radians
     this.rot = 0;
 
     //State / Utility Attributes
-    this.turnSpeed = 360; // Turn speed in degrees per second.
+    this.turnSpeed = TURN_SPEED; // Turn speed in degrees per second.
     this.exploding = false;
     this.explodeTime = 0;
-    this.friction = 0.7; // Level of friction (0 = none, 1 = Heavy)
-    this.thrustConst = 8; // Acceleration of the ship in pixels per second
+    this.explodeDuration = SHIP_EXPLODE_DURATION;
+    this.fps = FPS;
+    this.friction = FRICTION; // Level of friction (0 = none, 1 = Heavy)
+    this.thrustConst = SHIP_THRUST; // Acceleration of the ship in pixels per second
     this.thrusting = false;
     this.thrust = {
       x: 0,
@@ -33,13 +44,13 @@ class Ship {
   keyPress(ev) {
     switch (ev.key) {
       case 'ArrowLeft':
-        this.rot = ((360 / 180) * Math.PI) / 30; // TURN_SPEED / FPS
+        this.rot = ((this.turnSpeed / 180) * Math.PI) / this.fps; // TURN_SPEED / FPS
         break;
       case 'ArrowUp':
         this.thrusting = true;
         break;
       case 'ArrowRight':
-        this.rot = ((-360 / 180) * Math.PI) / 30; // TURN_SPEED / FPS
+        this.rot = ((-this.turnSpeed / 180) * Math.PI) / this.fps; // TURN_SPEED / FPS
         break;
     }
   }
@@ -57,14 +68,21 @@ class Ship {
     }
   }
 
-  update(FPS) {
+  explode() {
+    this.explodeTime = this.explodeDuration;
+  }
+
+  update() {
+    //Checking if the ship explosion time has gone off.
+    this.exploding = this.explodeTime > 0;
+
     //Thrust the ship
     if (this.thrusting) {
-      this.thrust.x += (this.thrustConst * Math.cos(this.a)) / FPS;
-      this.thrust.y -= (this.thrustConst * Math.sin(this.a)) / FPS;
+      this.thrust.x += (this.thrustConst * Math.cos(this.a)) / this.fps;
+      this.thrust.y -= (this.thrustConst * Math.sin(this.a)) / this.fps;
     } else {
-      this.thrust.x -= (this.friction * this.thrust.x) / FPS;
-      this.thrust.y -= (this.friction * this.thrust.y) / FPS;
+      this.thrust.x -= (this.friction * this.thrust.x) / this.fps;
+      this.thrust.y -= (this.friction * this.thrust.y) / this.fps;
     }
 
     // Update ship rotation
@@ -85,7 +103,7 @@ class Ship {
     if (!this.exploding) {
       // Beginning Draw triangle ship
       this.context.strokeStyle = 'white';
-      this.context.lineWidth = 30 / 20; // SHIP_SIZE / 20
+      this.context.lineWidth = this.size / 20; // SHIP_SIZE / 20
       this.context.beginPath();
 
       // Starting the stroke at the nose of the ship
