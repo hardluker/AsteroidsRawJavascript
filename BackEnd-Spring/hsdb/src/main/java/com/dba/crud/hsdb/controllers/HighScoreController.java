146aafaa1f74f4b57ea08dee69da2938dd96ee98
hardluker <dba9514@gmail.com>
2024-06-02 12:08:28 -0400

    Creating the controller for endpoints

--- BackEnd-Spring/hsdb/src/main/java/com/dba/crud/hsdb/controllers/HighScoreController.java
+++ BackEnd-Spring/hsdb/src/main/java/com/dba/crud/hsdb/controllers/HighScoreController.java
@@ -1,2 +1,22 @@
-package com.dba.crud.hsdb.controllers;public class HighScoreController {
+package com.dba.crud.hsdb.controllers;
+
+import com.dba.crud.hsdb.dtos.highScoreDto;
+import com.dba.crud.hsdb.services.HighScoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class HighScoreController {
+
+    private final HighScoreService highScoreService;
+
+    @GetMapping("/api/high-scores")
+    public ResponseEntity<List<highScoreDto>> allRecords() {
+        return ResponseEntity.ok(highScoreService.allRecords());
+    }
 }
