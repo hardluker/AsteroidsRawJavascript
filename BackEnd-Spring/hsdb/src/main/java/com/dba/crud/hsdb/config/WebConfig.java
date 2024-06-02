fbb6959bad1fdec32dc26976d7f087ae311f059a
hardluker <dba9514@gmail.com>
2024-06-02 13:20:50 -0400

    Added commenting for clarity

--- BackEnd-Spring/hsdb/src/main/java/com/dba/crud/hsdb/config/WebConfig.java
+++ BackEnd-Spring/hsdb/src/main/java/com/dba/crud/hsdb/config/WebConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+// This is only setup this way as the backend will be on the same server as the front end.
+// In a standard production environment, the backend would be on a different server but have the same origin.
 @Configuration
 public class WebConfig {
 
