d4bf6dc600fa8ef5efe5edc902e1cbe70c7c8e37
hardluker <dba9514@gmail.com>
2024-06-02 12:08:15 -0400

    Creating service for database queries

--- BackEnd-Spring/hsdb/src/main/java/com/dba/crud/hsdb/services/HighScoreService.java
+++ BackEnd-Spring/hsdb/src/main/java/com/dba/crud/hsdb/services/HighScoreService.java
@@ -1,2 +1,21 @@
-package com.dba.crud.hsdb.services;public class HighScoreService {
+package com.dba.crud.hsdb.services;
+
+import com.dba.crud.hsdb.dtos.highScoreDto;
+import com.dba.crud.hsdb.repository.HighScoresRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class HighScoreService {
+
+    private final HighScoresRepository highScoresRepository;
+
+    public List<highScoreDto> allRecords() {
+        return highScoresRepository.findAll();
+
+    }
 }
