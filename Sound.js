e9093845fd19400725856f9b733e58935dd98af9
hardluker <dba9514@gmail.com>
2024-06-01 21:17:41 -0400

    Updated comments

--- Sound.js
+++ Sound.js
@@ -1,19 +1,23 @@
 // This is a custom class to handle audio files and multiple streams.
-// This is necessary as Audio objects do not seem to be passable between files
 export class Sound {
   constructor(src, maxStreams = 1, vol = 1) {
     this.streamNum = 0;
     this.streams = [];
+
+    //Iterating through the streams array, adding audio, and setting volume.
     for (let i = 0; i < maxStreams; i++) {
       this.streams.push(new Audio(src));
       this.streams[i].volume = vol;
     }
 
+    //Playing the audio if it does not exceed the maxStreams of the instance.
+    // The modular arithmetic allows accessing the next item in the array if there is one.
     this.play = function () {
       this.streamNum = (this.streamNum + 1) % maxStreams;
       this.streams[this.streamNum].play();
     };
 
+    // This simply stops the current instance of the audio
     this.stop = function () {
       this.streams[this.streamNum].pause();
       this.streams[this.streamNum].currentTime = 0;
