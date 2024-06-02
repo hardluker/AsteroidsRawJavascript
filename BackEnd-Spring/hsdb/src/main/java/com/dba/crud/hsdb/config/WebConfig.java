beb0a6cc9533951603d866871e2fe21a5c7103c8
hardluker <dba9514@gmail.com>
2024-06-02 12:04:52 -0400

    Adding CORS configuration

--- BackEnd-Spring/hsdb/src/main/java/com/dba/crud/hsdb/config/WebConfig.java
+++ BackEnd-Spring/hsdb/src/main/java/com/dba/crud/hsdb/config/WebConfig.java
@@ -1,2 +1,22 @@
-package com.dba.crud.hsdb.config;public class WebConfig {
+package com.dba.crud.hsdb.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfig() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+                        .allowedOrigins("*");
+            }
+        };
+    }
 }
