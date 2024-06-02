674ccb2627222b22422ba0720e8c7a755177a0e9
hardluker <dba9514@gmail.com>
2024-06-02 12:06:49 -0400

    Creating the entity for the database

--- BackEnd-Spring/hsdb/src/main/java/com/dba/crud/hsdb/entities/HighScore.java
+++ BackEnd-Spring/hsdb/src/main/java/com/dba/crud/hsdb/entities/HighScore.java
@@ -1,2 +1,26 @@
-package com.dba.crud.hsdb.entities;public class highScore {
+package com.dba.crud.hsdb.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "high_scores")
+public class HighScore {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column
+    private String initial;
+
+    @Column
+    private Integer score;
 }
