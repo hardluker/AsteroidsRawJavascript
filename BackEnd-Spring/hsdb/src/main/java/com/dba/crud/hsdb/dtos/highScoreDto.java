6599605402cd0f39aebaf75602ac3d0c959880d5
hardluker <dba9514@gmail.com>
2024-06-02 12:06:34 -0400

    Creating the data transfer object

--- BackEnd-Spring/hsdb/src/main/java/com/dba/crud/hsdb/dtos/highScoreDto.java
+++ BackEnd-Spring/hsdb/src/main/java/com/dba/crud/hsdb/dtos/highScoreDto.java
@@ -1,2 +1,17 @@
-package com.dba.crud.hsdb.dtos;public class highScoreDto {
+package com.dba.crud.hsdb.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class highScoreDto {
+
+    private Long id;
+    private String initial;
+    private Integer score;
 }
