c115c23f7379820fc05aa70d5aa12d1e71a9e5cc
hardluker <dba9514@gmail.com>
2024-06-02 13:21:52 -0400

    Added the method for creating a high score in the DB

--- BackEnd-Spring/hsdb/src/main/java/com/dba/crud/hsdb/services/HighScoreService.java
+++ BackEnd-Spring/hsdb/src/main/java/com/dba/crud/hsdb/services/HighScoreService.java
@@ -1,6 +1,7 @@
 package com.dba.crud.hsdb.services;
 
 import com.dba.crud.hsdb.dtos.HighScoreDto;
+import com.dba.crud.hsdb.entities.HighScore;
 import com.dba.crud.hsdb.mappers.HighScoreMapper;
 import com.dba.crud.hsdb.repository.HighScoresRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,7 +16,17 @@ public class HighScoreService {
     private final HighScoresRepository highScoresRepository;
     private final HighScoreMapper highScoreMapper;
 
+    //This method is to query all records from the high score database.
     public List<HighScoreDto> allRecords() {
         return highScoreMapper.toHighScoreDtos(highScoresRepository.findAll());
+    }
+
+    //This method is to add an entry into the high score database.
+    public HighScoreDto createHighScore(HighScoreDto highScoreDto) {
+        HighScore highScore = highScoreMapper.toHighScore(highScoreDto);
+
+        HighScore createdHighScore = highScoresRepository.save(highScore);
+
+        return highScoreMapper.toHighScoreDto(createdHighScore);
     }
 }
