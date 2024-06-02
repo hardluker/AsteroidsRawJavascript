75fb883a4bde0ac12ef1e4912f665fb0504b3909
hardluker <dba9514@gmail.com>
2024-06-02 10:18:49 -0400

    Adding the files and dependencies for a spring boot application

---
+++ BackEnd-Spring/hsdb/src/main/java/com/dba/crud/hsdb/HsdbApplication.java
@@ -0,0 +1,13 @@
+package com.dba.crud.hsdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsdbApplication.class, args);
+	}
+
+}
---
+++ BackEnd-Spring/hsdb/src/test/java/com/dba/crud/hsdb/HsdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dba.crud.hsdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HsdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
