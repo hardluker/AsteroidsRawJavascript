93b228d8e219bb3256808902a96924fe0e1b9e19
hardluker <dba9514@gmail.com>
2024-06-02 16:37:10 -0400

    Created the entity

---
+++ BackEnd-Spring/src/main/java/com/dba9514/hsbackend/hsbackend/entities/User.java
@@ -0,0 +1,44 @@
+package com.dba9514.hsbackend.hsbackend.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "high-scores")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "initials")
+    private String initials;
+
+    @Column(name = "score")
+    private Integer score;
+
+    //Getters and Setters
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getInitials() {
+        return initials;
+    }
+
+    public void setInitials(String initials) {
+        this.initials = initials;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+
+
+
+
+}
