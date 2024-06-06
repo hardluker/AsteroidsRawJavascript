0cdda6b960fd9f473bba43944d682afce6b980cc
hardluker <dba9514@gmail.com>
2024-06-06 16:27:59 -0400

    Increased ship rotation speed and lower thrust speed

--- FrontEnd-Javascript/scripts/asteroid-game.js
+++ FrontEnd-Javascript/scripts/asteroid-game.js
@@ -10,9 +10,9 @@ export const AUDIO = false;
 // Ship Related Settings
 const SHIP_SIZE = 30; // Ship height in pixels.
 const SHIP_COLOR = 'white';
-const SHIP_THRUST = 3.6; // Acceleration of ship in pixels per second every second.
+const SHIP_THRUST = 3; // Acceleration of ship in pixels per second every second.
 const FRICTION = 0.7; // Friction coefficient of space (0 = no friction, 1 = ton of friction).
-const TURN_SPEED = 200; // Turn speed in degrees per second.
+const TURN_SPEED = 280; // Turn speed in degrees per second.
 const SHIP_EXPLODE_DURATION = 2; // Duration of ship explosion in seconds.
 const LASERS_MAX = 3; // Maximum number of lasers on the screen at once.
 const LASERS_SPEED = 400; // Speed of the lasers in pixels per second.
